fc28d1774b359773adb75251263d9b974db16602
Joe Daoud <joedaoud@outlook.com>
2016-08-01 22:37:23 -0700

    Configure views

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var
 
 app.set('view engine', 'ejs');
 
@@ -10,10 +9,11 @@ app.get('/', function(req, res){
 
 app.get('/artists', function(req, res){
   var artists = [
-    {name: "Kat Von D", image: "http://media.todaybirthdays.com/thumb_x256x256/upload/1982/03/08/kat-von-d.jpg"}
-    {name: "Don Ed Hardy", image: "http://images4.fanpop.com/image/photos/18500000/christian-audigier-and-ed-hardy-christian-audigier-and-ed-hardy-18588424-380-500.jpg"}
+    {name: "Kat Von D", image: "http://media.todaybirthdays.com/thumb_x256x256/upload/1982/03/08/kat-von-d.jpg"},
+    {name: "Don Ed Hardy", image: "http://images4.fanpop.com/image/photos/18500000/christian-audigier-and-ed-hardy-christian-audigier-and-ed-hardy-18588424-380-500.jpg"},
     {name: "Sailor Jerry", image: "https://img0.etsystatic.com/015/0/7011699/il_570xN.467689996_o7hj.jpg"}
   ]
+        res.render('artists', {artists:artists});
 });
 
 app.get('/shops', function(req, res){
